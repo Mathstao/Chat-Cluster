99b1e417e1b86a1ab45e25f57d302edad668a401
Mathstao <mathstao@163.com>
2018-04-04 20:49:23 +0800

    Update clusterAPI_startup.py

--- src/clusterAPI_startup.py
+++ src/clusterAPI_startup.py
@@ -1,4 +1,10 @@
 # -*- coding: UTF-8 -*-
+
+"""
+Author: mathstao
+Project: https://github.com/Mathstao/Chat-Cluster/
+"""
+
 import requests
 
 r = requests.get("http://172.18.163.200:5000/cluster/startup")
